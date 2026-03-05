b61c011e1fe2ed0e8e786fee662f5d40d037c7c3
sidrai <siddharth.rai@gmail.com>
2026-03-05 22:20:55 +1100

    change is key to create diff state object

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -3,7 +3,7 @@ module "asg_app" {
 
   environment = "dev"
   ami_id      = "ami-0818a4d7794d429b1"  # Amazon Ubuntu ap-southeast-2
-  key_name    = "MyCDKKey"
+  key_name    = "MyCDKKey"  # Different state file from dev
 
   asg_min_size         = 2
   asg_max_size         = 4
